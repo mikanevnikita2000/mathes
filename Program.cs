b04eb4711b316d72e12fccb65000ccfbabda05a6
mikanevnikita2000 <86831041+mikanevnikita2000@users.noreply.github.com>
2021-09-28 16:50:28 +0300

    1 commit

--- Program.cs
+++ Program.cs
@@ -10,6 +10,307 @@ namespace mather_primere
     {
         static void Main(string[] args)
         {
+            bool isEnabled = false;
+            string questions;
+            while (isEnabled)
+            {
+                Console.WriteLine("Выбери какие примеры тебе нужны:");
+                Console.WriteLine("1. Сложение");
+                Console.WriteLine("2. Вычитание");
+                Console.WriteLine("3. Умножение");
+                Console.WriteLine("4. Деление");
+                Console.WriteLine("5. Выход");
+                string num = Console.ReadLine();
+                Random gpg = new Random();
+                bool isCorrect = false;
+
+                AdditionAndSubtractionAndMultiplication(num);
+
+                /*while (isEnabled == false)
+                {
+
+                }
+
+                if (num == "4")
+                {
+                }
+            }
+            isHided = true;
+            if (num == "5")
+            {
+                isHided = true;
+            }*/
+            }
+        }
+
+        static void AdditionAndSubtractionAndMultiplication(string num,bool isHided)
+        {
+            Random gpg = new Random();
+            int exampleresponse;
+            int expectedResult;
+            string generetedExpression;
+            string questions;
+            bool isTaskCorrect = true;//ToDo пример сделан правильно или нет
+            int a = gpg.Next(0, 1000);
+            int b = gpg.Next(0, 1000);
+
+            if (num == "1")
+            {
+                generetedExpression = a + "+" + b;
+                exampleresponse = a + b;
+                isTaskCorrect = test(generetedExpression, exampleresponse);
+            }
+            if (num == "2")
+            {
+                if (expectedResult == a - b)
+                {
+
+                    Console.WriteLine($"сколько будет: {a} - {b}");
+                    exampleresponse = a - b;
+                }
+            }
+            if (num == "3")
+            {
+                if (expectedResult == a * b)
+                {
+
+                    Console.WriteLine($"сколько будет: {a} * {b}");
+                    exampleresponse = a * b;
+                }
+            }
+            int TheCorrectNessofTheAnswer = 0;
+            while (TheCorrectNessofTheAnswer == 0)
+            {
+                int expectedResult = (Convert.ToInt32(Console.ReadLine()));
+                if (expectedResult == exampleresponse)
+                {
+                    TheCorrectNessofTheAnswer = 1;
+                    Console.WriteLine("Молодец, правильно!");
+                }
+            }
+            Console.WriteLine("Хочешь ещё пример?");
+            questions = Console.ReadLine();
+            if (questions == "Нет" || questions == "нет")
+            {
+                isHided = true;
+            }
+            return;
+
+        }
+        //метод принимает на вход пример принимает результат и отдаёт на выходе bool
+        //ToDo можно ограничить колличество итераций проверки 
+        static bool test(string visibleExpression, int expectedResult)
+        {
+
+            bool result = false;
+            Console.WriteLine($"сколько будет: {visibleExpression}");
+            while (!result)
+            {
+                if (expectedResult == (Convert.ToInt32(Console.ReadLine())))
+                {
+                    result = true;
+                    Console.WriteLine("Молодец! Правильно!");
+                }
+                else
+                {
+                    Console.WriteLine("попробуй ещё раз");
+                }
+            }
+            return result;
+        }
+        static bool TheDivisionMethod(int a, int b)
+        {
+            string questions;
+            while (isEnabled == false)
+            {
+                int prom1 = gpg.Next(0, 9);
+                if (prom1 == 0)
+                {
+                    int prom2 = gpg.Next(0, 99);
+                    while (isCorrect == false)
+                    {
+                        Console.WriteLine($"сколько будет: {prom2} : 1");
+                        int expectedResult = (Convert.ToInt32(Console.ReadLine()));
+                        if (expectedResult == prom2)
+                        {
+                            Console.WriteLine("Молодец! Правильно!");
+                            isCorrect = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("попробуй ещё раз");
+                        }
+                    }
+                }
+                if (prom1 == 1)
+                {
+                    int[] loc = { 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+                    int prom2 = gpg.Next(0, 9);
+                    int n = loc[prom2];
+                    while (isCorrect == false)
+                    {
+                        Console.WriteLine($"сколько будет: {n} : 2");
+                        int expectedResult = (Convert.ToInt32(Console.ReadLine()));
+                        if (expectedResult == n / 2)
+                        {
+                            Console.WriteLine("Молодец! Правильно!");
+                            isCorrect = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("попробуй ещё раз");
+                        }
+                    }
+                }
+                if (prom1 == 2)
+                {
+                    int[] loc = { 6, 9, 12, 15, 18, 21, 24, 28, 30 };
+                    int prom2 = gpg.Next(0, 9);
+                    int n = loc[prom2];
+                    while (isCorrect == false)
+                    {
+                        Console.WriteLine($"сколько будет: {n} : 3");
+                        int expectedResult = (Convert.ToInt32(Console.ReadLine()));
+                        if (expectedResult == n / 3)
+                        {
+                            Console.WriteLine("Молодец! Правильно!");
+                            isCorrect = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("попробуй ещё раз");
+                        }
+                    }
+                }
+                if (prom1 == 3)
+                {
+                    int[] loc = { 8, 12, 16, 20, 24, 28, 32, 36, 40 };
+                    int prom2 = gpg.Next(0, 9);
+                    int n = loc[prom2];
+                    while (isCorrect == false)
+                    {
+                        Console.WriteLine($"сколько будет: {n} : 4");
+                        int expectedResult = (Convert.ToInt32(Console.ReadLine()));
+                        if (expectedResult == n / 4)
+                        {
+                            Console.WriteLine("Молодец! Правильно!");
+                            isCorrect = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("попробуй ещё раз");
+                        }
+                    }
+                }
+                if (prom1 == 4)
+                {
+                    int[] loc = { 10, 15, 20, 25, 30, 35, 40, 45, 50 };
+                    int prom2 = gpg.Next(0, 9);
+                    int n = loc[prom2];
+                    while (isCorrect == false)
+                    {
+                        Console.WriteLine($"сколько будет: {n} : 5");
+                        int expectedResult = (Convert.ToInt32(Console.ReadLine()));
+                        if (expectedResult == n / 5)
+                        {
+                            Console.WriteLine("Молодец! Правильно!");
+                            isCorrect = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("попробуй ещё раз");
+                        }
+                    }
+                }
+                if (prom1 == 5)
+                {
+                    int[] loc = { 12, 18, 24, 30, 36, 42, 48, 54, 60 };
+                    int prom2 = gpg.Next(0, 9);
+                    int n = loc[prom2];
+                    while (isCorrect == false)
+                    {
+                        Console.WriteLine($"сколько будет: {n} : 6");
+                        int expectedResult = (Convert.ToInt32(Console.ReadLine()));
+                        if (expectedResult == n / 6)
+                        {
+                            Console.WriteLine("Молодец! Правильно!");
+                            isCorrect = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("попробуй ещё раз");
+                        }
+                    }
+                }
+                if (prom1 == 6)
+                {
+                    int[] loc = { 14, 21, 28, 35, 42, 49, 56, 63, 70 };
+                    int prom2 = gpg.Next(0, 9);
+                    int n = loc[prom2];
+                    while (isCorrect == false)
+                    {
+                        Console.WriteLine($"сколько будет: {n} : 7");
+                        int expectedResult = (Convert.ToInt32(Console.ReadLine()));
+                        if (expectedResult == n / 7)
+                        {
+                            Console.WriteLine("Молодец! Правильно!");
+                            isCorrect = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("попробуй ещё раз");
+                        }
+                    }
+                }
+                if (prom1 == 7)
+                {
+                    int[] loc = { 16, 24, 32, 40, 48, 56, 64, 72, 80 };
+                    int prom2 = gpg.Next(0, 9);
+                    int n = loc[prom2];
+                    while (isCorrect == false)
+                    {
+                        Console.WriteLine($"сколько будет: {n} : 8");
+                        int expectedResult = (Convert.ToInt32(Console.ReadLine()));
+                        if (expectedResult == n / 8)
+                        {
+                            Console.WriteLine("Молодец! Правильно!");
+                            isCorrect = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("попробуй ещё раз");
+                        }
+                    }
+                }
+                if (prom1 == 8)
+                {
+                    int[] loc = { 18, 27, 36, 45, 54, 63, 72, 81, 90 };
+                    int prom2 = gpg.Next(0, 9);
+                    int n = loc[prom2];
+                    while (isCorrect == false)
+                    {
+                        Console.WriteLine($"сколько будет: {n} : 9");
+                        int expectedResult = (Convert.ToInt32(Console.ReadLine()));
+                        if (expectedResult == n / 9)
+                        {
+                            Console.WriteLine("Молодец! Правильно!");
+                            isCorrect = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("попробуй ещё раз");
+                        }
+                    }
+                }
+                isCorrect = false;
+                Console.WriteLine("Хочешь ещё пример?");
+                questions = Console.ReadLine();
+                if (questions == "Нет" || questions == "нет")
+                {
+                    isEnabled = true;
+                }
+                return isEnabled;
+            }
         }
     }
 }
